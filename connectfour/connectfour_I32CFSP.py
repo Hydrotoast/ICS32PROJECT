22780f3a8dd823cebc8f0e956095792f50566fc5
Bohyun Kim <bohyunk@uci.edu>
2014-04-20 21:48:49 -0700

    add connectfour files

---
+++ connectfour/connectfour_I32CFSP.py
@@ -0,0 +1,29 @@
+import socket
+
+
+def connect_socket(user_host: str, user_port: int, user_id: str) -> socket:
+    s = socket.socket()
+    try:
+        s.connect((user_host, user_port))
+        socket_in = s.makefile('r')
+        socket_out = s.makefile('w')
+        socket_out.write('I32CFSP_HELLO '+user_id+'\r\n')
+        socket_out.flush()
+        reply = socket_in.readline()
+        print(reply)
+        socket_out.write('AI_GAME\r\n')
+        socket_out.flush()
+        reply = socket_in.readline()
+        print(reply)
+    except:
+        print('Connection Failed. Closing the socket.')
+    finally:
+        s.close()
+    
+#OfficeHour question: what functions do I need for this module?
+
+if __name__ == '__main__':
+    user_host = input('Please specify IP address or a host')
+    user_port = int(input('Please enter port'))
+    user_id = input('Please enter your user id').strip()
+    connect_socket(user_host, user_port, user_id)
